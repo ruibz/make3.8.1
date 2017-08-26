9203c8ec783c51ff9f35c1e5ce49d88dfa9a557c
ruibz <rui.b.zhang@hotmail.com>
2017-08-26 10:30:43 +0800

    test

--- main.c
+++ main.c
@@ -2968,11 +2968,11 @@ print_data_base ()
   printf (_("\n# Make data base, printed on %s"), ctime (&when));
 
   print_variable_data_base ();
-  print_dir_data_base ();
-  print_rule_data_base ();
-  print_file_data_base ();
-  print_vpath_data_base ();
-  strcache_print_stats ("#");
+  //print_dir_data_base ();
+  //print_rule_data_base ();
+  //print_file_data_base ();
+  //print_vpath_data_base ();
+  //strcache_print_stats ("#");
 
   when = time ((time_t *) 0);
   printf (_("\n# Finished Make data base on %s\n"), ctime (&when));
